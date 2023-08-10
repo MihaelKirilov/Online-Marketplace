ac56b03c6e8aef76520c2173439f0e62052ad106
MihaelKirilov <mvencikirilov@gmai.com>
2023-08-10 18:28:21 +0300

    second

---
+++ online_market/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn online_market.wsgi --log-file -
--- online_market/online_market/settings.py
+++ online_market/online_market/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-&=y)cw%-z1hod0((g_0!bwh7s-2t1mt2yiqf^gcv7jg591mzuc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL ='/'
 LOGOUT_REDIRECT_URL = '/'
@@ -119,6 +120,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+
+
+
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
